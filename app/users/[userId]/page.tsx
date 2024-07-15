d6b106363b5dbaed613331dcd720189dcdefd48c
JohnMoody12 <123786549+JohnMoody12@users.noreply.github.com>
2024-07-15 17:32:56 -0500

    updates

---
+++ app/users/[userId]/page.tsx
@@ -0,0 +1,59 @@
+import getUser from "@/lib/getUser";
+import getUserPosts from "@/lib/getUserPosts";
+import { Suspense } from "react";
+import UserPosts from "./components/UserPosts";
+import type { Metadata } from "next";
+import Link from "next/link";
+import getAllUsers from "@/lib/getAllUsers";
+import { notFound } from "next/navigation";
+
+type Params = {
+  params: {
+    userId: string;
+  };
+};
+
+export async function generateMetadata({
+  params: { userId },
+}: Params): Promise<Metadata> {
+  const userData: Promise<User> = getUser(userId);
+  const user: User = await userData;
+  if (!user.name) {
+    return {
+      title: "User Not Found",
+    };
+  }
+  return {
+    title: user.name,
+    description: `This is the page of ${user.name}`,
+  };
+}
+
+export default async function UserPage({ params: { userId } }: Params) {
+  const userData: Promise<User> = getUser(userId);
+  const userPostsData: Promise<Post[]> = getUserPosts(userId);
+
+  //const [user, userPosts] = await Promise.all([userData, userPostsData]);
+  const user = await userData;
+
+  return (
+    <>
+      <h2>
+        <Link href="/users">Back to Users</Link>
+      </h2>
+      <br />
+      <h2>{user.name}</h2>
+      <br />
+      <Suspense fallback={<h2>Loading...</h2>}>
+        <UserPosts promise={userPostsData} />
+      </Suspense>
+    </>
+  );
+}
+
+export async function generateStaticParams() {
+  const usersData: Promise<User[]> = getAllUsers();
+  const users = await usersData;
+
+  return users.map((user) => ({ userId: user.id.toString() }));
+}
